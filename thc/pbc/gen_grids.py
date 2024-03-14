4cdadd112e129a43acabb736eb36c8d8af603e9a
Junjie Yang <yangjunjie0320@gmail.com>
2024-03-14 14:08:13 +0800

    pbc grid method

--- thc/pbc/gen_grids.py
+++ thc/pbc/gen_grids.py
@@ -8,11 +8,11 @@ from pyscf.dft import numint
 from pyscf import dft, lib
 import pyscf.dft.gen_grid
 
-from pyscf.dft.gen_grid import (nwchem_prune, gen_atomic_grids,
-                                BLKSIZE, NBINS, CUTOFF)
+from pyscf.dft.gen_grid import nwchem_prune, gen_atomic_grids
 from pyscf.pbc.gto import eval_gto as pbc_eval_gto
-
 libpbc = lib.load_library('libpbc')
+
+import thc
 
 # modified from pyscf.dft.gen_grid.gen_partition and pyscf.pbc.dft.gen_grid.get_becke_grids
 def gen_partition(cell, atom_grid={}, radi_method=dft.radi.gauss_chebyshev,
@@ -46,7 +46,6 @@ def gen_partition(cell, atom_grid={}, radi_method=dft.radi.gauss_chebyshev,
     tol = 1e-15
     coord_grid = []
     weigh_grid = []
-
     coord_atm_img = []
 
     for ia in range(cell.natm):
@@ -86,10 +85,8 @@ def gen_partition(cell, atom_grid={}, radi_method=dft.radi.gauss_chebyshev,
 
                 c_ia.append(c)
                 w_ia.append(w)
-                
                 coord_atm_img.append(coord_atom[iL, ia])
-        
-        # print(c_ia, w_ia)
+
         c_ia = numpy.vstack(c_ia)
         w_ia = numpy.hstack(w_ia)
 
@@ -99,7 +96,10 @@ def gen_partition(cell, atom_grid={}, radi_method=dft.radi.gauss_chebyshev,
     coord_atm_img = numpy.vstack(coord_atm_img)
     p_radii_table = lib.c_null_ptr()
 
-    def gen_grid_partition(c):
+    coord_all = []
+    weigh_all = []
+
+    for ia, (c, w) in enumerate(zip(coord_grid, weigh_grid)):
         c  = numpy.asarray(c, order='C')
         ng = c.shape[0]
         na = coord_atm_img.shape[0]
@@ -111,154 +111,101 @@ def gen_partition(cell, atom_grid={}, radi_method=dft.radi.gauss_chebyshev,
             coord_atm_img.ctypes.data_as(ctypes.c_void_p),
             p_radii_table, ctypes.c_int(na), ctypes.c_int(ng)
         )
+        w /= p.sum(axis=0)
 
-        return p
-
-    coord_all = []
-    weigh_all = []
-
-    for ia in range(cell.natm):
-        c_ia = coord_grid[ia]
-        w_ia = weigh_grid[ia]
-
-        p_ia = gen_grid_partition(c_ia)
-        w_ia /= p_ia.sum(axis=0)
-
-        coord_all.append(c_ia)
-        weigh_all.append(w_ia)
+        coord_all.append(c)
+        weigh_all.append(w)
 
     if concat:
         coord_all = numpy.vstack(coord_all)
         weigh_all = numpy.hstack(weigh_all)
     
-    print(coord_all, weigh_all)
+    return coord_all, weigh_all
 
+class InterpolatingPoints(thc.mol.InterpolatingPoints):
+    def __init__(self, cell):
+        self.cell = cell
+        thc.mol.InterpolatingPoints.__init__(self, cell)
 
+    get_partition = gen_partition
 
-    # offs = numpy.append(0, numpy.cumsum([w.size for w in weights_all]))
+    def make_mask(self, cell, coords, relativity=0, shls_slice=None, verbose=None):
+        if cell is None: cell = self.cell
+        if coords is None: coords = self.coords
+        from pyscf.pbc.dft.gen_grid import make_mask
+        return make_mask(cell, coords, relativity, shls_slice, verbose)
 
-    # coords_all = numpy.vstack(coords_all)
-    # weighs_all = numpy.hstack(weighs_all)
+    def build(self, cell=None, with_non0tab=False):
+        '''
+        Build ISDF grids.
+        '''
+        if cell is None: cell = self.mol
+        log = logger.new_logger(self, self.verbose)
+        log.info('\nSet up ISDF grids with QR decomposition.')
+        if self.c_isdf is not None:
+            log.info('c_isdf = %d', self.c_isdf)
 
-    # atm_coords = numpy.asarray(atm_coords.reshape(-1,3)[supatm_idx], order='C')
-    # sup_natm = len(atm_coords)
-    # p_radii_table = lib.c_null_ptr()
-    # fn = dft.gen_grid.libdft.VXCgen_grid
-    # ngrids = weights_all.size
+        grid = zip(
+            cell.aoslice_by_atom(),
+            *self.get_partition(
+                cell, self.atom_grid,
+                radi_method=self.radi_method,
+                level=self.level,
+                prune=self.prune,
+                concat=False
+            )
+        )
 
-    # for iL, L in enumerate(Ls):
-    #     for ia in range(cell.natm):
-    #         coords, vol = atom_grids_tab[cell.atom_symbol(ia)]
-    #         coords = coords + atm_coords[iL, ia]
+        cput0 = (logger.process_clock(), logger.perf_counter())
 
-    # # max_memory = cell.max_memory - lib.current_memory()[0]
-    # # blocksize = min(ngrids, max(2000, int(max_memory*1e6/8 / sup_natm)))
-    # # displs = lib.misc._blocksize_partition(offs, blocksize)
+        coords = []
+        weights = []
 
-    # # for n0, n1 in zip(displs[:-1], displs[1:]):
-    # #     p0, p1 = offs[n0], offs[n1]
-    # #     pbecke = numpy.empty((sup_natm,p1-p0))
-    # #     coords = numpy.asarray(coords_all[p0:p1], order='F')
-    # #     fn(pbecke.ctypes.data_as(ctypes.c_void_p),
-    # #        coords.ctypes.data_as(ctypes.c_void_p),
-    # #        atm_coords.ctypes.data_as(ctypes.c_void_p),
-    # #        p_radii_table, ctypes.c_int(sup_natm), ctypes.c_int(p1-p0))
+        for ia, (s, c, w) in enumerate(grid):
+            sym = cell.atom_symbol(ia)
+            nao = s[3] - s[2]
 
-    # #     weights_all[p0:p1] /= pbecke.sum(axis=0)
-    # #     for ia in range(n0, n1):
-    # #         i0, i1 = offs[ia], offs[ia+1]
-    # #         weights_all[i0:i1] *= pbecke[ia,i0-p0:i1-p0]
-
-    # # return coords_all, weights_all
-
-# class InterpolatingPoints(pyscf.dft.gen_grid.Grids):
-#     tol = 1e-8
-#     c_isdf = 50
-#     alignment = 0
-    
-#     def build(self, mol=None, with_non0tab=False, sort_grids=True, **kwargs):
-#         '''
-#         Build ISDF grids.
-#         '''
-#         if mol is None: mol = self.mol
-#         log = logger.new_logger(self, self.verbose)
-#         log.info('\nSet up ISDF grids with QR decomposition.')
-#         if self.c_isdf is not None:
-#             log.info('c_isdf = %d', self.c_isdf)
-
-#         atom_grids_tab = self.gen_atomic_grids(
-#             mol, self.atom_grid, self.radi_method,
-#             self.level, self.prune, **kwargs
-#         )
-
-#         grid = zip(
-#             mol.aoslice_by_atom(),
-#             *self.get_partition(
-#                 mol, atom_grids_tab, self.radii_adjust,
-#                 self.atomic_radii, self.becke_scheme,
-#                 concat=False
-#             )
-#         )
-
-#         cput0 = (logger.process_clock(), logger.perf_counter())
-
-#         coords = []
-#         weights = []
-
-#         for ia, (s, c, w) in enumerate(grid):
-#             sym = mol.atom_symbol(ia)
-#             nao = s[3] - s[2]
-
-#             phi  = numint.eval_ao(mol, c, deriv=0, shls_slice=None)
-#             phi *= (numpy.abs(w) ** 0.5)[:, None]
-#             phi  = phi[numpy.linalg.norm(phi, axis=1) > self.tol]
-#             ng   = phi.shape[0]
+            phi  = numint.eval_ao(cell, c, deriv=0, shls_slice=None)
+            phi *= (numpy.abs(w) ** 0.5)[:, None]
+            phi  = phi[numpy.linalg.norm(phi, axis=1) > self.tol]
+            ng   = phi.shape[0]
             
-#             phi_pair  = numpy.einsum("xm,xn->xmn", phi, phi[:, s[2]:s[3]])
-#             phi_pair  = phi_pair.reshape(ng, -1)
+            phi_pair  = numpy.einsum("xm,xn->xmn", phi, phi[:, s[2]:s[3]])
+            phi_pair  = phi_pair.reshape(ng, -1)
 
-#             q, r, perm = scipy.linalg.qr(phi_pair.T, pivoting=True)
-#             diag = (lambda d: d / d[0])(numpy.abs(numpy.diag(r)))
+            q, r, perm = scipy.linalg.qr(phi_pair.T, pivoting=True)
+            diag = (lambda d: d / d[0])(numpy.abs(numpy.diag(r)))
 
-#             nip = int(self.c_isdf) * nao if self.c_isdf else ng
-#             nip = min(nip, ng)
+            nip = int(self.c_isdf) * nao if self.c_isdf else ng
+            nip = min(nip, ng)
 
-#             mask = numpy.where(diag > self.tol)[0]
-#             mask = mask if len(mask) < nip else mask[:nip]
-#             nip = len(mask)
+            mask = numpy.where(diag > self.tol)[0]
+            mask = mask if len(mask) < nip else mask[:nip]
+            nip = len(mask)
 
-#             ind = perm[mask]
-#             coords.append(c[ind])
-#             weights.append(w[ind])
+            ind = perm[mask]
+            coords.append(c[ind])
+            weights.append(w[ind])
 
-#             log.info(
-#                 "Atom %d %s: nao = % 4d, %6d -> %4d, err = % 6.4e" % (
-#                     ia, sym, nao, w.size, nip, diag[mask[-1]]
-#                 )
-#             )
+            log.info(
+                "Atom %d %s: nao = % 4d, %6d -> %4d, err = % 6.4e" % (
+                    ia, sym, nao, w.size, nip, diag[mask[-1]]
+                )
+            )
 
-#         log.timer("Building Interpolating Points", *cput0)
+        log.timer("Building Interpolating Points", *cput0)
 
-#         self.coords  = numpy.vstack(coords)
-#         self.weights = numpy.hstack(weights)
+        self.coords  = numpy.vstack(coords)
+        self.weights = numpy.hstack(weights)
 
-#         if sort_grids:
-#             from pyscf.dft.gen_grid import arg_group_grids
-#             ind = arg_group_grids(mol, self.coords)
-#             self.coords = self.coords[ind]
-#             self.weights = self.weights[ind]
+        if with_non0tab:
+            self.non0tab = self.make_mask(cell, self.coords)
+            self.screen_index = self.non0tab
+        else:
+            self.screen_index = self.non0tab = None
 
-#         if self.alignment > 1:
-#             raise KeyError("Alignment is not supported for ISDF grids.")
-
-#         if with_non0tab:
-#             self.non0tab = self.make_mask(mol, self.coords)
-#             self.screen_index = self.non0tab
-#         else:
-#             self.screen_index = self.non0tab = None
-
-#         log.info('Total number of interpolating points = %d', len(self.weights))
-#         return self
+        log.info('Total number of interpolating points = %d', len(self.weights))
+        return self
 
 # Grids = InterpolatingPoints
 
@@ -277,14 +224,9 @@ if __name__ == "__main__":
     cell.basis = "sto3g"
     cell.build()
 
-    gen_partition(
-        cell, concat=False
-    )
-
-
-    # grid = InterpolatingPoints(m)
-    # grid.level = 0
-    # grid.verbose = 6
-    # grid.c_isdf  = 20
-    # grid.tol     = 1e-8
-    # grid.kernel()
+    grid = InterpolatingPoints(m)
+    grid.level = 0
+    grid.verbose = 6
+    grid.c_isdf  = 20
+    grid.tol     = 1e-8
+    grid.kernel()
